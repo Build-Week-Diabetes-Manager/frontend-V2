ea2b523f85187295885189bca53dbdb7ea5c012f
Zach Young <zachyoung@Zachs-MacBook-Pro.local>
2020-05-07 19:14:09 -0500

    styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,18 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom'
 import '../App.css';
-import logo from '../assets/logo-wo-text-thick.svg'
+import logo from '../assets/logo-v2.svg'
 function Nav() {
   return (
 <div className="nav">
   <div className="nav-left"><ul>
-    <li><img src={logo} alt=""/></li>
-  <li><Link to='/'>Home</Link></li>
+    {/* <li><img src={logo} alt=""/></li> */}
+    <li><Link to='/'>Home</Link></li>
       <li><Link to='/dashboard'>Dashboard</Link></li>
-      </ul></div>
+      </ul>
+      </div>
       <div className="nav-middle">
-        <h2>INSULINE</h2>
+        <img src={logo} alt=""/>
       </div>
   <div className="nav-right">
     <div className="logout">Logout</div>
