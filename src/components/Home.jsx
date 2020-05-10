48ca7d373a23fac7630fc97b6be0f53a83c2680c
Zach Young <zachyoung@Zachs-MacBook-Pro.local>
2020-05-10 01:06:49 -0500

    forms

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Search from './Search'
+
 
 function Home() {
   return (
-    <div className="Footer">
+    <div className="Home">
+      <Search/>
     </div>
   );
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,21 @@
 import React, {useState} from 'react';
 import {Link} from 'react-router-dom'
-import '../App.css';
+import {UserOutlined} from '@ant-design/icons'
 import logo from '../assets/logo-v2.svg'
 function Nav() {
   return (
 <div className="nav">
   <div className="nav-left"><ul>
     {/* <li><img src={logo} alt=""/></li> */}
-    <li><Link to='/'>Home</Link></li>
-      <li><Link to='/dashboard'>Dashboard</Link></li>
+    <li><Link className='link' to='/'>Home</Link></li>
+      <li><Link className='link' to='/dashboard'>Dashboard</Link></li>
       </ul>
       </div>
       <div className="nav-middle">
         <img src={logo} alt=""/>
       </div>
   <div className="nav-right">
+  <UserOutlined />
     <div className="logout">Logout</div>
   </div>
   </div>
